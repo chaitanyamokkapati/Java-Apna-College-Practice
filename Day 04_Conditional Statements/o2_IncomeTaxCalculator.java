8f7b650231e50a5a5970499587db79366463945f
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-18 20:39:23 +0530

    Added TAx calculator

---
+++ Day 04_Conditional Statements/o2_IncomeTaxCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class o2_IncomeTaxCalculator {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        System.out.print("Enter your Income: ");
+        int income_user = user_input.nextInt();
+        int tax ;
+
+        if (income_user < 500000) {
+            tax = 0;
+        }
+        else if (income_user >= 500000 && income_user >= 1000000) {
+            tax = (int) (income_user * 0.2);
+        }
+        else{
+            tax = (int) (income_user * 0.3);
+        }
+
+        System.out.println("Your tax is : " + tax + ".");
+    }
+    
+}
