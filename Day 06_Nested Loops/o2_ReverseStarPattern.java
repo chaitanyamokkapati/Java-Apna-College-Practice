971220f93f404dc9a23e225751c302cb6706f12d
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-19 21:37:44 +0530

    Added o2_ReverseStarPattern

---
+++ Day 06_Nested Loops/o2_ReverseStarPattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class o2_ReverseStarPattern {
+    public static void main(String[] args) {
+        Scanner user_in = new Scanner(System.in);
+        System.out.print("Enter PAttern Value: ");
+        int num = user_in.nextInt();
+
+        for(int star = 1; star <= num; star++){
+            for(int line = 1; line <= num - star + 1; line++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        user_in.close();
+    }
+}
