31cc3f910c1912f7f600e334d5d2f31f74126ee8
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-19 10:20:12 +0530

    Added While Loop

---
+++ Day 05_Loops Flow Control/o1_WhileLoop.java
@@ -0,0 +1,9 @@
+public class o1_WhileLoop {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.println(counter + ". " + "Hello, World!!");
+            counter++;
+        }
+    }
+}
