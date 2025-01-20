03cd44771d1b0ff8694d57139bee9817d4b1f65a
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-20 10:53:36 +0530

    Added o1_syntaxFunction

---
+++ Day 07_Functions and Methods/o1_syntaxFunction.java
@@ -0,0 +1,14 @@
+// Write a Function that says hello world to user 4 Times.
+
+public class o1_syntaxFunction {
+    public static void Greet() {
+        System.out.println("Hello, World!!");
+        System.out.println("Hello, World!!");
+        System.out.println("Hello, World!!");
+        System.out.println("Hello, World!!");
+        return;
+    }
+    public static void main(String[] args) {
+        Greet();
+    }
+}
