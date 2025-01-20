2ae5e0e42da5119f418329685f6f11edb20993c1
chaitanyamokkapati <chaitanyamokkapati6@gmail.com>
2025-01-20 14:43:16 +0530

    Added o5_Product

---
+++ Day 07_Functions and Methods/o5_Product.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class o5_Product {
+    public static int prod(int a, int b) {
+        int Product = a * b;
+        return Product;
+    }
+    public static void main(String[] args) {
+        Scanner user_in = new Scanner(System.in);
+        System.out.print("Pleae Enter a : ");
+        int a = user_in.nextInt();
+        System.out.print("Pleae Enter b : ");
+        int b = user_in.nextInt();
+
+        int ans = prod(a, b);
+        //System.out.println(prod(a, b));
+
+        System.out.println("Product of " + a + " * " + b +" = " + ans);
+        
+        user_in.close();
+    }
+    
+}
